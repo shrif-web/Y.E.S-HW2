5034a283f8fed02ed7ad4cb8a30206a62dcaba33
baharkhd <baharkh127@gmail.com>
2020-11-22 02:23:56 +0330

    merged all files

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,11 +1,6 @@
 function onButtonClick() {
   console.log("on button clicked!");
-  console.log(document.getElementById("enterButton"));
-  if (document.getElementById("enterButton").classList.contains("inverted")) {
-    document.getElementById("enterButton").classList.remove("inverted");
-  } else {
-    document.getElementById("enterButton").classList.add("inverted");
-  }
+  // location.href = "../data.html";
 }
 
 function onLoad() {
